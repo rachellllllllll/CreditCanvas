2c5f7ce72048f9ebed62a88dbb93dedd03f650c6
Rachel <r0527124976@gmail.com>
2025-11-20 11:44:50 +0200

    fix XLSX

--- vite.config.ts
+++ vite.config.ts
@@ -9,12 +9,4 @@ export default defineConfig({
       '/api': 'http://localhost:3001',
     },
   },
-  optimizeDeps: {
-    include: ["xlsx"]
-  },
-  build: {
-    commonjsOptions: {
-      include: [/xlsx/]
-    }
-  }
 });
