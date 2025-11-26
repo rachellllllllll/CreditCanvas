8d31bda1d214e34014b7ea580e047ae3ae91fc1d
rachellllllllll <92730911+rachellllllllll@users.noreply.github.com>
2025-11-26 09:34:53 +0200

    Update xlsx.d.ts

--- src/xlsx.d.ts
+++ src/xlsx.d.ts
@@ -1,5 +1,14 @@
 declare module 'xlsx' {
-  export const read: any;
-  export const write: any;
-  export const utils: any;
+  export interface WorkBook {
+    SheetNames: string[];
+    Sheets: { [sheet: string]: any };
+  }
+  
+  export const read: (data: any, opts?: any) => WorkBook;
+  export const write: (wb: WorkBook, opts?: any) => any;
+  
+  export const utils: {
+    sheet_to_json: (sheet: any, opts?: any) => any[];
+    encode_cell: (cell: { c: number; r: number }) => string;
+  };
 }
