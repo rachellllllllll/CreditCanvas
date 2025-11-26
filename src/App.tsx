566d1da84b8ed8239b6aa7ae8c66438b40bbda86
Rachel <r0527124976@gmail.com>
2025-11-26 09:11:01 +0200

    fix import to import * as XLSX from xlsx;

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,5 @@
 import React, { useState } from 'react';
-import XLSX from 'xlsx';
+import * as XLSX from 'xlsx';
 import type { CreditDetail, AnalysisResult } from './types';
 import CategoryManager, { type CategoryDef } from './components/CategoryManager';
 import SettingsMenu from './components/SettingsMenu';
--- src/utils/bankParser.ts
+++ src/utils/bankParser.ts
@@ -1,4 +1,4 @@
-import XLSX from 'xlsx';
+import * as XLSX from 'xlsx';
 import type { CreditDetail } from '../types';
 
 function normalizeDate(input: any): string {
